683ecaf0015e57b62b273600fbf6223231500314
Bogban893 <bogbanrvd@gmail.com>
2026-04-30 19:32:31 +0300

    add args to main and runSequential

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,33 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+import kotlinx.cli.required
+import sequential.runSequential
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+fun main(args: Array<String>) {
+    val parser = ArgParser("convolution")
+
+    val image by parser.option(
+        ArgType.String,
+        shortName = "i",
+        description = "Input image path"
+    ).required()
+
+    val kernel by parser.option(
+        ArgType.String,
+        shortName = "k",
+        description = "Kernel file path"
+    ).required()
+
+    val output by parser.option(
+        ArgType.String,
+        shortName = "o",
+        description = "Output image path",
+        fullName = "output"
+    ).default("output.png")
+    parser.parse(args)
+
+    runSequential(image, kernel, output)
 }
